60a70d04a327fb87fe0d5c9330e17b3635d46a5b
𝚅𝙾𝙻𝚃𝙰𝙶𝙴 𝙻𝙾𝚁𝙳 𝙳𝙴𝚅 <easynero6@gmail.com>
2025-05-29 21:55:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
+//==BANK,CHANGE DETAILS TO YOURS===//
 global.bankname = "Opay"
 global.banknumber = "7045989058"
 global.bankowner = "ᴀsᴋ ғᴏʀ ɴᴀᴍᴇ"
-//==========  CPANEL COMMAND ===============\\
-global.domain = 'https://queenstech.anonymoushosting.com.ng' // Fill in your domain, don't put a / at the end of the link
-global.apikey = 'ptla_lPDiNBSpqCQ9ctLs7qh5MCqfp0QYnJ9ehuOMQxMNqnm' // Fill Apikey
-global.capikey = 'ptlc_qHq1Jq1xrnhv7v95n65tNsM7awsHJYyj0NJ3yd0VNKt' // Fill Apikey
-//=========================================================//
+//==========  CPANEL COMMAND FILL BOTH ===============\\
+global.domain = '_' // Fill in your domain, don't put a / at the end of the link
+global.apikey = '_' // Fill APIKEY
+global.capikey = '_' // Fill Apikey
+//===FILL THIS WITH EITHER ANOTHER ADMIN PANEL PR SAME ONE=====//
 //Server create panel egg pm2
-global.apikey2 = 'ptla_lPDiNBSpqCQ9ctLs7qh5MCqfp0QYnJ9ehuOMQxMNqnm' // Fill Apikey
-global.capikey2 = 'ptlc_qHq1Jq1xrnhv7v95n65tNsM7awsHJYyj0NJ3yd0VNKt' // Fill Apikey
-global.domain2 = 'https://queenstech.anonymoushosting.com.ng' // Fill Domain
+global.apikey2 = '_' // Fill Apikey
+global.capikey2 = '_' // Fill Apikey
+global.domain2 = '_' // Fill Domain
 global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //don't change it
 
 global.eggsnya2 = '15' // ID of eggs used
